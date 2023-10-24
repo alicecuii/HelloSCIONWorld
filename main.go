078be7c55ef9d8d8c2a8594e0950d970f36fa1b9
alice <aalicecuii@gmail.com>
2023-10-24 04:22:36 +0000

    edit main.go

--- main.go
+++ main.go
@@ -134,6 +134,7 @@ func runClient(address string, count int, policy pan.Policy) error {
 		fmt.Println("server address error")
 		return err
 	}
+	fmt.Println("policy: %s", policy)
 	//Select path to control connection
 	pathSelector := pan.NewDefaultSelector()
 	conn, err := pan.DialUDP(context.Background(), netaddr.IPPort{}, addr, policy, pathSelector)
